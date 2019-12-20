42b1c63d0961d4b6aaabc386615e9a4dd35c62e0
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 02:34:38 -0400

    Activar el bluetooth del dispositivo

--- app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.practicandobluetooth
 
 import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter() // llama al adaptador bluetooth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
             Toast.makeText(this, "El dispositivo no soporta bluetooth",Toast.LENGTH_SHORT)
+        }
+
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1) // lo correcto era REQUEST_ENABLE_BT
         }
     }
 
