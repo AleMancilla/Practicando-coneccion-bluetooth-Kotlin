197bc9a85a4aaabc8901503b47d29427f5e171f2
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 02:09:24 -0400

    Verifica que exista el adaptador bluetooth

--- app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.practicandobluetooth
 
+import android.bluetooth.BluetoothAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Toast.makeText(this, "El dispositivo no soporta bluetooth",Toast.LENGTH_SHORT)
+        }
     }
+
+
 }
