44fe760d577b34777aff526413f069f20bc954b1
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 15:34:33 -0400

    enlistar dispositivos bluetooth en un list view

--- app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.practicandobluetooth
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,11 +35,48 @@ class MainActivity : AppCompatActivity() {
             val event = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(event,REQUEST_ENABLE_BLUETOOTH)
         }
+        id_button_refresh.setOnClickListener{pairedDeviceList()}
     }
 
     private fun pairedDeviceList()
     {
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+        if(!m_pairedDevices.isEmpty())
+        {
+            for (device: BluetoothDevice in m_pairedDevices)
+            {
+                list.add(device)
+            }
+        }
+        else
+        {
+            Toast.makeText(this,"No se encontraron dispositivos enparejados",Toast.LENGTH_SHORT).show()
+        }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,list)
+        id_listView.adapter = adapter
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                if(m_bluetoothAdapter!!.isEnabled)
+                {
+                    Toast.makeText(this,"El bluetooth ha sido habilitado",Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(this,"El bluetooth ha sido Deshabilitado",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else if(resultCode == Activity.RESULT_CANCELED)
+            {
+                Toast.makeText(this,"La coneccion Bluetooth ha sido cancelada",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
