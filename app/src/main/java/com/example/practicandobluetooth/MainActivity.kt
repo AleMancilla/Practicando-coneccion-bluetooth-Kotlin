0bf7ca95711eb6eb625d4dc621bf11a1ece8d97b
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 11:32:22 -0400

    A empezar de nuevo

--- app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
@@ -8,21 +8,11 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter() // llama al adaptador bluetooth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-            Toast.makeText(this, "El dispositivo no soporta bluetooth",Toast.LENGTH_SHORT)
-        }
 
-        if (bluetoothAdapter?.isEnabled == false) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, 1) // lo correcto era REQUEST_ENABLE_BT
-        }
     }
 
 
