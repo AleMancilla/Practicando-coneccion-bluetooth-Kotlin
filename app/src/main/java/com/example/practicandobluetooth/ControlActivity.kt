b6434026fb8c4fd23ebe53008969edb1f8f6a1ad
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 16:59:27 -0400

    Conectarse a dispositivo

--- app/src/main/java/com/example/practicandobluetooth/ControlActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/ControlActivity.kt
@@ -1,12 +1,62 @@
 package com.example.practicandobluetooth
 
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.control_layout.*
+import java.util.*
 
 class ControlActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    companion object{
+        var m_myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var m_bluetooth_Socket : BluetoothSocket? = null
+        lateinit var m_progress: ProgressDialog
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        var m_isConnected: Boolean = false
+        lateinit var m_adrres: String
     }
 
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.control_layout)
+
+        m_adrres = intent.getStringExtra(MainActivity.EXTRA_ADDRESS)
+        ConnectToDevice(this).execute()
+
+        //id_button_on.setOnClickListener { sendcomand() }
+    }
+
+    private fun sendcomand(input: String){
+
+    }
+    private fun dissconect(){
+
+    }
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String> (){
+
+        private var connectSuccess: Boolean = true
+        private val context: Context
+
+        init{
+            this.context = c
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: Void?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+    }
 }
