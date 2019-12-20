73f23aeaccd36d529e4594818995f9da211b6b85
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-12-20 13:10:11 -0400

    verifica si el dispositivo es compatible con bluetooth

--- app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/practicandobluetooth/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practicandobluetooth
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +9,29 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var m_bluetoothAdapter: BluetoothAdapter
+    lateinit var m_pairedDevices: Set<BluetoothDevice>
+    var REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val EXTRA_ADDRESS: String = "Devisse Address"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(m_bluetoothAdapter == null) // verifica si el dispositivo es compatible con bluetooth
+        {
+            Toast.makeText(this,"Dispositivo no compatible con bluetooth",Toast.LENGTH_SHORT).show()
+            return
+        }
     }
 
+    private fun pairedDeviceList()
+    {
+
+    }
 
 }
